a49e2e39fcf2e7f2ac644572ef75ea1ec1b657f7
Baraa Jayousi <b.jayousi98@gmail.com>
2024-05-05 15:54:31 +0300

    Setup basic template for authentication pages

--- CarBookProject/settings.py
+++ CarBookProject/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'auth_app',
+    'crispy_forms',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
