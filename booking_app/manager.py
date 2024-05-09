0554934cbfe099723aae24c5fb9e415962f3ef3a
Baraa Jayousi <b.jayousi98@gmail.com>
2024-05-09 19:21:32 +0300

    added cars page

--- booking_app/manager.py
+++ booking_app/manager.py
@@ -3,3 +3,6 @@ from django.db.models import Manager
 class CarManager(Manager):
   def get_featured_cars(self):
     return self.filter(featured=True)
+  
+  def get_all_cars(self):
+    return self.all()
--- booking_app/views.py
+++ booking_app/views.py
@@ -5,9 +5,21 @@ from .models import Car
 
 class HomePage(View):
   view_template = "index.html"
-  context ={}
+  context = {}
   def get(self, request):
     self.context['featured_cars'] = Car.objects.get_featured_cars()
     return render(request, self.view_template, self.context)
+  
+
+class CarsPage(View):
+  view_template = "car.html"
+  context = {}
+  def get(self, request):
+    self.context['cars'] = Car.objects.get_all_cars()
+    return render(request, self.view_template, self.context)
+  
+
+class CarDetailsPage(View):
+  view_template = ""
 
 
