38aec356d60fa32b64f1c37880ae776c0a93fe93
Baraa Jayousi <b.jayousi98@gmail.com>
2024-05-11 00:47:41 +0300

    started with car booking page (checkout)

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -5,6 +5,7 @@ app_name='booking_app'
 urlpatterns = [ 
   path('', views.HomePage.as_view(), name="home_page"),
   path('cars/', views.CarsPage.as_view(), name="cars_page"),
-  path('cars/<int:id>', views.CarDetailsPage.as_view(), name="car_details")
+  path('cars/<int:id>', views.CarDetailsPage.as_view(), name="car_details"),
+  path('cars/book', views.CarBookPage.as_view(), name="car_book")
 ]
 
